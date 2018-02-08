05d1d9be43c76045a1fab55b9056f8f9d08aa86b
haronius <aaron.wilso@gmail.com>
2018-02-07 21:03:43 -0700

    Restructured peices and create board

--- src/Board.hs
+++ src/Board.hs
@@ -3,6 +3,8 @@ module Board where
 import Data.Map as M
 import Prelude as P
 import Control.Lens as L
+
+board :: [String]
 board = ["◻◼◻◼◻◼◻◼"
     , "◼◻◼◻◼◻◼◻"
     , "◻◼◻◼◻◼◻◼"
@@ -14,60 +16,58 @@ board = ["◻◼◻◼◻◼◻◼"
     ]
 
 
--- data ChessPieces = Map [Char] (Char, (Integer, Integer))
-    
-initPieces :: Map String (Char, (Int, Int))
-initPieces = M.fromList [("rk1", ('♜', (0, 0)))
-            , ("rk2", ('♜', (7, 0)))
-            , ("rk3", ('♖', (0, 7)))
-            , ("rk4", ('♖', (7, 7)))
-
-            , ("kn1", ('♞', (1, 0)))
-            , ("kn2", ('♞', (6, 0)))
-            , ("kn3", ('♘', (1, 7)))
-            , ("kn4", ('♘', (6, 7)))
-
-            , ("bp1", ('♝', (2, 0)))
-            , ("bp2", ('♝', (5, 0)))
-            , ("bp3", ('♗', (2, 7)))
-            , ("bp4", ('♗', (5, 7)))
-
-            , ("kg1", ('♛', (3, 0)))
-            , ("kg2", ('♕', (3, 7)))
-
-            , ("qn1", ('♚', (4, 0)))
-            , ("qn2", ('♔', (4, 7)))
-
-            , ("pn1", ('♟', (0, 1)))
-            , ("pn2", ('♟', (1, 1)))
-            , ("pn3", ('♟', (2, 1)))
-            , ("pn4", ('♟', (3, 1)))
-            , ("pn5", ('♟', (4, 1)))
-            , ("pn6", ('♟', (5, 1)))
-            , ("pn7", ('♟', (6, 1)))
-            , ("pn8", ('♟', (7, 1)))
-
-            , ("pn9", ('♙', (0, 6)))
-            , ("pn10", ('♙', (1, 6)))
-            , ("pn11", ('♙', (2, 6)))
-            , ("pn12", ('♙', (3, 6)))
-            , ("pn13", ('♙', (4, 6)))
-            , ("pn14", ('♙', (5, 6)))
-            , ("pn15", ('♙', (6, 6)))
-            , ("pn16", ('♙', (7, 6)))
+initPieces :: Map (Int, Int) Char
+initPieces = M.fromList [((0, 0), '♜')
+    , ((7, 0), '♜')
+    , ((0, 7), '♖')
+    , ((7, 7), '♖')
+    , ((1, 0), '♞')
+    , ((6, 0), '♞')
+    , ((1, 7), '♘')
+    , ((6, 7), '♘')
+    , ((2, 0), '♝')
+    , ((5, 0), '♝')
+    , ((2, 7), '♗')
+    , ((5, 7), '♗')
+    , ((3, 0), '♛')
+    , ((3, 7), '♕')
+    , ((4, 0), '♚')
+    , ((4, 7), '♔')
+    , ((0, 1), '♟')
+    , ((1, 1), '♟')
+    , ((2, 1), '♟')
+    , ((3, 1), '♟')
+    , ((4, 1), '♟')
+    , ((5, 1), '♟')
+    , ((6, 1), '♟')
+    , ((7, 1), '♟')
+    , ((0, 6), '♙')
+    , ((1, 6), '♙')
+    , ((2, 6), '♙')
+    , ((3, 6), '♙')
+    , ((4, 6), '♙')
+    , ((5, 6), '♙')
+    , ((6, 6), '♙')
+    , ((7, 6), '♙')
     ]
 
 
-createBoard :: [String] -> Map String (Char, (Int, Int)) -> [String] -> [String] 
-createBoard b p [] = b
-createBoard b p ks = createBoard (place b (p ! (head ks))) p (P.drop 1 ks)
+createBoard :: [String] -> Map (Int, Int) Char -> [(Int, Int)] -> [String] 
+createBoard b m ks = P.foldl (\a k -> let
+    y = snd k
+    x = fst k
+    row = a !! y
+    p = m ! k
+    newRow = (element x .~ p) row
+        in (element y .~ newRow) a
+    ) b ks
 
-place :: [String] -> (Char, (Int, Int)) -> [String]
-place b p = let
-    y = snd . snd $ p
-    c = fst p
-    row = (!!) b y
-    x = fst . snd $ p
-    newRow = (element x .~ c) row
-    in 
-        (element y .~ newRow) b
+
+-- place :: [String] -> (Int, Int)
+-- place a k = let
+--     y = snd k
+--     x = fst k
+--     row = a !! y
+--     p = l
+--     newRow = (element x .~ p) row
+--         in (element y .~ newRow) a
